0f4bb597fb86af7fba2cbe8db22ea79062e1f0d8
filipewelton1 <filipewelton1@gmail.com>
2022-12-16 08:32:59 -0300

    feat: services

---
+++ src/services/user.ts
@@ -0,0 +1,73 @@
+import { ExceptionError } from '@shared/helpers/exceptionError'
+import { UserModel } from '@models/user'
+
+export class UserService {
+  public async create(data: Creation) {
+    try {
+      await new UserModel(data)
+        .save()
+    } catch (error) {
+      throw new ExceptionError('THIRD_PARTY_SERVICE_ERROR', error)
+    }
+  }
+
+  public async get(fieldName: string, fieldValue: string) {
+    try {
+      const user = await UserModel.findOne({ [fieldName]: fieldValue })
+
+      if (user === null) {
+        return Promise.reject(new ExceptionError('NOT_FOUND_ERROR', 'User not found'))
+      }
+
+      return user
+    } catch (error) {
+      throw new ExceptionError('THIRD_PARTY_SERVICE_ERROR', error)
+    }
+  }
+
+  public async checkIfDoesNotExist(email: string) {
+    try {
+      await this.get('email', email)
+      return Promise.reject(new ExceptionError('CONFLICT_ERROR', 'This user already exists'))
+    } catch (error) {
+      const err: ExceptionError = error
+
+      if (err.type === 'NOT_FOUND_ERROR') {
+        return Promise.resolve()
+      }
+
+
+      throw error
+    }
+  }
+
+  public async updateGroupList(userId: string, groupId: string) {
+    try {
+      await UserModel.findByIdAndUpdate(userId, {
+        $push: {
+          groups: [groupId],
+        },
+      })
+    } catch (error) {
+      throw new ExceptionError('THIRD_PARTY_SERVICE_ERROR', error)
+    }
+  }
+
+  public async updateContactCards(userId: string, contactCardId: string) {
+    try {
+      await UserModel.findByIdAndUpdate(userId, {
+        $push: {
+          contactCards: [contactCardId],
+        },
+      })
+    } catch (error) {
+      throw new ExceptionError('THIRD_PARTY_SERVICE_ERROR', error)
+    }
+  }
+
+  public async validateHash(storedHash: string, providedHash: string) {
+    if (storedHash !== providedHash) {
+      throw new ExceptionError('FORBIDDEM_ERROR', 'Invalid hash')
+    }
+  }
+}
---
+++ src/services/userProfile.ts
@@ -0,0 +1,48 @@
+import { ExceptionError } from '@shared/helpers/exceptionError'
+import { UserProfileModel } from '@models/userProfile'
+
+export class UserProfileService {
+  public async create(data: Creation) {
+    try {
+      const profile = new UserProfileModel(data)
+      await profile.save()
+      return profile.id
+    } catch (error) {
+      throw new ExceptionError('THIRD_PARTY_SERVICE_ERROR', error)
+    }
+  }
+
+  public async get(profileId: string): Promise<UserProfileSchema> {
+    try {
+      const profile = await UserProfileModel.findById(profileId)
+
+      if (profile === null) {
+        return Promise.reject(
+          new ExceptionError('NOT_FOUND_ERROR', 'User profile not found')
+        )
+      }
+
+      return profile
+    } catch (error) {
+      throw new ExceptionError('THIRD_PARTY_SERVICE_ERROR', error)
+    }
+  }
+
+  public async update(profileId: string, data: Update) {
+    try {
+      await UserProfileModel.findByIdAndUpdate(profileId, {
+        $set: data,
+      })
+    } catch (error) {
+      throw new ExceptionError('THIRD_PARTY_SERVICE_ERROR', error)
+    }
+  }
+
+  public async exclude(profileId: string): Promise<void> {
+    try {
+      await UserProfileModel.findByIdAndDelete(profileId)
+    } catch (error) {
+      throw new ExceptionError('THIRD_PARTY_SERVICE_ERROR', error)
+    }
+  }
+}
